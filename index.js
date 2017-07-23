b5e81e0b7e834dc5dc8bf39127f1e96e21b43d9c
Ubuntu <ubuntu@ip-172-31-0-231.us-east-2.compute.internal>
2017-07-23 21:30:38 +0000

    create html and connect nodejs

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
 const PORT_NUMBER = 8080
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + '/templates/mymap.html'));
 })
 
 app.listen(8080, function () {
