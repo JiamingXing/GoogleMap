2fa2f3e98f46502746daad0e73afe4954c85b593
Jiaming Xing <jx703@nyu.edu>
2017-09-17 22:36:15 -0700

    Make our code not depend on google map api

--- js/mymap.js
+++ js/mymap.js
@@ -1,35 +1,42 @@
 var DEFAULT_ZOOM = 15;
 var GOOGLE_API_KEY = 'AIzaSyAAerCc7M_7gAWfRE1_y_Ze8deCw2JwRZg';
 var DEFAULT_RADIUS = 500;
+//New York city location
+var DEFAULT_LAT = 40.785091;
+var DEFAULT_LNG = -73.968285;
 
-function initMap() {
-   var position = {
-	lat : 40.785091,
-	lng : -73.968285
-	}
-   var map = new google.maps.Map($('#map')[0], {
+$(function() {
+  function initMap() {
+    var position = {
+      lat: DEFAULT_LAT,
+      lng: DEFAULT_LNG
+    };
+
+    var map = new google.maps.Map($('#map')[0], {
       zoom: DEFAULT_ZOOM,
       center: position
-   });
-   var marker = new google.maps.Marker({
-     position: position,
-     map: map
-   });
+    });
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map
+    });
 
+    $.ajax({
+      url: '/nearby_search',
+      data: {
+        'location': position.lat + ',' + position.lng,
+        'type': 'restaurant',
+        'key': GOOGLE_API_KEY,
+        'radius': DEFAULT_RADIUS
+      },
+      success: function(data) {
+        debugger;
+      },
+      failure: function(data) {
+        debugger;
+      }
+    });
+  }
 
-   $.ajax({
-    url: '/nearby_search',
-    data: {
-      'location': position.lat + ',' + position.lng,
-      'type': 'restaurant',
-      'key': GOOGLE_API_KEY,
-      'radius': DEFAULT_RADIUS
-    },
-    success: function(data){ 
-      debugger;
-    },
-    failure: function(data) {
-      debugger;
-    }
-  });
-}
+  initMap();
+});
