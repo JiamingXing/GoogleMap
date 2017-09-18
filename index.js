ba6536d7af9850f5ff946fb4495962e4199b49fa
Ubuntu <ubuntu@ip-172-31-0-231.us-east-2.compute.internal>
2017-09-18 03:54:14 +0000

    test

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const express = require('express');
+const request = require('request');
 const app = express()
 
-const PORT_NUMBER = 8080
+
+const PORT_NUMBER = 8080;
+const GOOGLE_KEY = 'AIzaSyAAerCc7M_7gAWfRE1_y_Ze8deCw2JwRZg';
+const NEARBY_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/templates/mymap.html'));
 })
+
+app.get('/nearby_search', function (req, api_res) {
+  var radius = req.query.radius ? req.query.radius : 150;
+  var params = {
+    'key': GOOGLE_KEY,
+    'location': req.query.location,
+    'radius': radius,
+    'type': req.query.type
+  };
+  request({url: NEARBY_SEARCH_URL, qs: params}, function(err, res, body) {
+    if (res.statusCode == 200) {
+      api_res.json(body);
+    } else {
+      // error handler
+    }
+  });
+});
 
 app.use('/css', express.static('css'))
 app.use('/js', express.static('js'))
--- js/mymap.js
+++ js/mymap.js
@@ -1,11 +1,34 @@
+var DEFAULT_ZOOM = 15;
+var GOOGLE_API_KEY = 'AIzaSyAAerCc7M_7gAWfRE1_y_Ze8deCw2JwRZg';
+
 function initMap() {
-   var uluru = {lat: -25.363, lng: 131.044};
+   var position = {
+	lat : 40.785091,
+	lng : -73.968285
+	}
    var map = new google.maps.Map($('#map')[0], {
-      zoom: 4,
-      center: uluru
+      zoom: DEFAULT_ZOOM,
+      center: position
    });
    var marker = new google.maps.Marker({
-     position: uluru,
+     position: position,
      map: map
    });
- }
+
+
+   $.ajax({
+    url: '/nearby_search',
+    data: {
+      'location': position.lat + ',' + position.lng,
+      'type': 'restaurant',
+      'key': GOOGLE_API_KEY,
+      'radius': 500
+    },
+    success: function(data){ 
+      debugger;
+    },
+    failure: function(data) {
+      debugger;
+    }
+  });
+}
